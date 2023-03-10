f1952a2fa26604a87d6b19e5c02a7b6b7405ef41
unknown <hubballisiddaraj@gmail.com>
2023-03-10 15:20:17 +0530

    question level score calculation

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 import scraper.scrape as scrape
+import preprocessor as preprocessor
 
-print("==================== SEARCH ENGINE ====================")
+print("\n                 ==================== SEARCH ENGINE ====================\n")
+question = input("Search : ")
 
-search_term = input("Search : ")
 
-print("Fetching links for the keyword " + search_term + "...")
-links = scrape.get_links(search_term)
-print(links)
+# assigning weight to find level of the question
+question_terms = preprocessor.cleanup_stopwords(question)
+print(question_terms)
 
-print("Fetching data from the links...")
-content = scrape.get_data(links)
-print("\n")
-print(content)
+basic_keywords = ['define', 'list', 'identify', 'describe','who', 'where', 'when', 'what', 'which']
+intermediate_keywords = ['compare', 'contrast', 'analyze', 'evaluate','why', 'how']
+advanced_keywords = ['critique', 'hypothesize', 'propose', 'explore', 'justify']
+
+score = 0
+for word in question_terms:
+    if word in basic_keywords:
+        score = score + 2
+    elif word in intermediate_keywords:
+        score = score + 5
+    elif word in advanced_keywords:
+        score = score + 10
+    else: 
+        score = score + 1
+    
+
+
+# print("Fetching links for the keyword " + question + "...")
+# links = scrape.get_links(question)
+# print(links)
+
+# print("Fetching data from the links...")
+# content = scrape.get_data(links)
+# print("\n")
+# print(content)
+
+
+
+
+
