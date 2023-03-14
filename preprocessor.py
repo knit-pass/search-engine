9d56c5c6900ea241a8163350392da0c78e3cd617
Prajwal Patil <prajwalpatilk@gmail.com>
2023-03-14 13:32:29 +0530

    Update gitignore

--- preprocessor.py
+++ preprocessor.py
@@ -2,9 +2,11 @@ import nltk
 # nltk.download("stopwords")      #run this once
 from nltk.corpus import stopwords
 
+
 def cleanup_stopwords(s):
     required = ['who', 'where', 'when', 'why', 'what', 'which', 'how']
     stopset = set(stopwords.words('english')) - set(required)
     tokens = nltk.word_tokenize(s)
-    cleanup = [token.lower()for token in tokens if token.lower() not in stopset and  len(token)>2]
+    cleanup = [token.lower()for token in tokens if token.lower()
+               not in stopset and len(token) > 2]
     return cleanup
